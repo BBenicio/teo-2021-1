33ff0b3a2a2d7986b9712480357eafe46e579679
Bruno Benicio <brunobenicio2214@gmail.com>
2021-10-20 23:20:58 -0300

    generate initial solution before calling metaheuristics

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from cvrp_input import prepare_input
+from greedy import greedy
 from local_search import local_search
 from utils import calculate_cost, is_valid
 import metaheuristics
@@ -12,15 +13,16 @@ def run_mh(files: 'list[str]') -> 'list[dict]':
     D, demands, Q = prepare_input(files[0])
     
     # compila os métodos para não interferir tanto no tempo
+    route, start = greedy(D, demands, Q, alpha=0)
     metaheuristics.grasp(D, demands, Q, alpha=0, non_improving_iter=0)
-    metaheuristics.ils(D, demands, Q, non_improving_iter=0)
-    metaheuristics.simulated_annealing(D, demands, Q, T_max=0.01, alpha=0.99)
+    metaheuristics.ils(route, start, D, demands, Q, non_improving_iter=0)
+    metaheuristics.simulated_annealing(route, start, D, demands, Q, T_max=0.01, alpha=0.99)
 
     for filepath in files:
         print(filepath)
         D, demands, Q = prepare_input(filepath)
         t0 = time()
-        route, start = metaheuristics.grasp(D, demands, Q, alpha=0.5, non_improving_iter=4000)
+        route, start = metaheuristics.grasp(D, demands, Q, alpha=0.3, non_improving_iter=4000)
         t1 = time()
         metadata.append({
             'instance': filepath,
@@ -32,8 +34,9 @@ def run_mh(files: 'list[str]') -> 'list[dict]':
             'valid': is_valid(route, start, demands, Q)
         })
 
+        route, start = greedy(D, demands, Q, alpha=0.3)
         t0 = time()
-        route, start = metaheuristics.ils(D, demands, Q, non_improving_iter=5000)
+        route, start = metaheuristics.ils(route, start, D, demands, Q, k=5, non_improving_iter=5000)
         t1 = time()
         metadata.append({
             'instance': filepath,
@@ -45,9 +48,9 @@ def run_mh(files: 'list[str]') -> 'list[dict]':
             'valid': is_valid(route, start, demands, Q)
         })
 
+        route, start = greedy(D, demands, Q, alpha=0.3)
         t0 = time()
-        route, start = metaheuristics.simulated_annealing(D, demands, Q, T_max=10, T_min=1, alpha=0.99)
-        route, start = local_search(route, start, D, demands, Q)
+        route, start = metaheuristics.simulated_annealing(route, start, D, demands, Q, T_max=10, T_min=1, alpha=0.99)
         t1 = time()
         metadata.append({
             'instance': filepath,
@@ -62,7 +65,8 @@ def run_mh(files: 'list[str]') -> 'list[dict]':
     return metadata
 
 if __name__ == '__main__':
-    files = glob.glob('../A-VRP/*.vrp')
+    # files = glob.glob('./A-VRP/*.vrp')
+    files = ['./A-VRP/A-n32-k5.vrp']
     meta = run_mh(files)
     df = pd.DataFrame(meta)
     df.to_csv('results.csv', sep='\t')
