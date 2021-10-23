df7107b0c537343cedd565af80b440082c2d9b7c
Bruno Benicio <brunobenicio2214@gmail.com>
2021-10-23 02:09:49 -0300

    prepare main.py for running experiments

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from cvrp_input import prepare_input
 from greedy import greedy
-from local_search import local_search
 from utils import calculate_cost, is_valid
 import metaheuristics
 from time import time
@@ -8,59 +7,108 @@ import glob
 import pandas as pd
 
 
-def run_mh(files: 'list[str]') -> 'list[dict]':
+def run_mh(files: 'list[str]', grasp=True, ils=True, simulated_annealing=True, tabu_search=True, grasp_tabu=True, ils_tabu=True, iters=1) -> 'list[dict]':
     metadata = []
     D, demands, Q = prepare_input(files[0])
     
-    # compila os métodos para não interferir tanto no tempo
-    route, start = greedy(D, demands, Q, alpha=0)
-    metaheuristics.grasp(D, demands, Q, alpha=0, non_improving_iter=0)
-    metaheuristics.ils(route, start, D, demands, Q, non_improving_iter=0)
-    metaheuristics.simulated_annealing(route, start, D, demands, Q, T_max=0.01, alpha=0.99)
-
     for filepath in files:
         print(filepath)
         D, demands, Q = prepare_input(filepath)
-        t0 = time()
-        route, start = metaheuristics.grasp(D, demands, Q, alpha=0.3, non_improving_iter=4000)
-        t1 = time()
-        metadata.append({
-            'instance': filepath,
-            'metaheuristic': 'GRASP',
-            'time': t1-t0,
-            'cost': calculate_cost(route, start, D),
-            'route': route,
-            'start': start,
-            'valid': is_valid(route, start, demands, Q)
-        })
+        for i in range(iters):
+            if grasp:
+                t0 = time()
+                route, start = metaheuristics.grasp(D, demands, Q, alpha=0.3, non_improving_iter=4 * D.shape[0])
+                t1 = time()
+                metadata.append({
+                    'instance': filepath,
+                    'metaheuristic': 'GRASP',
+                    'iteration': i,
+                    'time': t1-t0,
+                    'cost': calculate_cost(route, start, D),
+                    'route': route,
+                    'start': start,
+                    'valid': is_valid(route, start, demands, Q)
+                })
 
-        route, start = greedy(D, demands, Q, alpha=0.3)
-        t0 = time()
-        route, start = metaheuristics.ils(route, start, D, demands, Q, k=5, non_improving_iter=5000)
-        t1 = time()
-        metadata.append({
-            'instance': filepath,
-            'metaheuristic': 'ILS',
-            'time': t1-t0,
-            'cost': calculate_cost(route, start, D),
-            'route': route,
-            'start': start,
-            'valid': is_valid(route, start, demands, Q)
-        })
+            if ils:
+                route, start = greedy(D, demands, Q, alpha=0.3)
+                t0 = time()
+                route, start = metaheuristics.ils(route, start, D, demands, Q, k=5, non_improving_iter=4 * D.shape[0])
+                t1 = time()
+                metadata.append({
+                    'instance': filepath,
+                    'metaheuristic': 'ILS',
+                    'iteration': i,
+                    'time': t1-t0,
+                    'cost': calculate_cost(route, start, D),
+                    'route': route,
+                    'start': start,
+                    'valid': is_valid(route, start, demands, Q)
+                })
 
-        route, start = greedy(D, demands, Q, alpha=0.3)
-        t0 = time()
-        route, start = metaheuristics.simulated_annealing(route, start, D, demands, Q, T_max=10, T_min=1, alpha=0.99)
-        t1 = time()
-        metadata.append({
-            'instance': filepath,
-            'metaheuristic': 'Simulated Annealing',
-            'time': t1-t0,
-            'cost': calculate_cost(route, start, D),
-            'route': route,
-            'start': start,
-            'valid': is_valid(route, start, demands, Q)
-        })
+            if simulated_annealing:
+                route, start = greedy(D, demands, Q, alpha=0.3)
+                t0 = time()
+                route, start = metaheuristics.simulated_annealing(route, start, D, demands, Q, T_max=2000, T_min=0.1, alpha=0.95)
+                t1 = time()
+                metadata.append({
+                    'instance': filepath,
+                    'metaheuristic': 'Simulated Annealing',
+                    'iteration': i,
+                    'time': t1-t0,
+                    'cost': calculate_cost(route, start, D),
+                    'route': route,
+                    'start': start,
+                    'valid': is_valid(route, start, demands, Q)
+                })
+            
+            if tabu_search:
+                route, start = greedy(D, demands, Q, alpha=0.3)
+                t0 = time()
+                route, start = metaheuristics.do_tabu_search(route, start, D, demands, Q)
+                t1 = time()
+                metadata.append({
+                    'instance': filepath,
+                    'metaheuristic': 'Tabu Search',
+                    'iteration': i,
+                    'time': t1-t0,
+                    'cost': calculate_cost(route, start, D),
+                    'route': route,
+                    'start': start,
+                    'valid': is_valid(route, start, demands, Q)
+                })
+            
+            if grasp_tabu:
+                t0 = time()
+                route, start = metaheuristics.grasp_tabu(D, demands, Q, alpha=0.3, non_improving_iter=D.shape[0])
+                t1 = time()
+                metadata.append({
+                    'instance': filepath,
+                    'metaheuristic': 'GRASP Tabu',
+                    'iteration': i,
+                    'time': t1-t0,
+                    'cost': calculate_cost(route, start, D),
+                    'route': route,
+                    'start': start,
+                    'valid': is_valid(route, start, demands, Q)
+                })
+
+            if ils_tabu:
+                route, start = greedy(D, demands, Q, alpha=0.3)
+                t0 = time()
+                route, start = metaheuristics.ils_tabu(route, start, D, demands, Q, k=5, non_improving_iter=D.shape[0])
+                t1 = time()
+                metadata.append({
+                    'instance': filepath,
+                    'metaheuristic': 'ILS Tabu',
+                    'iteration': i,
+                    'time': t1-t0,
+                    'cost': calculate_cost(route, start, D),
+                    'route': route,
+                    'start': start,
+                    'valid': is_valid(route, start, demands, Q)
+                })
+        
 
     return metadata
 
