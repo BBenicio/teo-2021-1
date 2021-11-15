3106fc6ae6ade54d789c98cf80425a628c8ad028
Bruno Benicio <brunobenicio2214@gmail.com>
2021-11-15 07:46:24 -0300

    document local search and operators, remove relocate operator

--- local_search.py
+++ local_search.py
@@ -1,19 +1,31 @@
+import numpy as np
 from numba import njit, types
 from operators import aggregate
 from utils import calculate_cost, is_valid
 
 @njit(types.Tuple((types.int32[::1], types.boolean[::1]))(types.int32[::1], types.boolean[::1], types.int32[:, ::1], types.int32[::1], types.int64))
-def local_search(route, start, D, demands, Q):
+def local_search(route: np.ndarray, start: np.ndarray, D: np.ndarray, demands: np.ndarray, Q: int):
+    '''Encontra o ótimo local percorrendo a vizinhança da solução.
+
+    A cada iteração, é selecionado o melhor vizinho até que não seja possível
+    obter uma solução melhor.
+    '''
     best_cost = calculate_cost(route, start, D)
     best_sol = (route, start)
+
     cost = best_cost
+    
+    # enquanto o último vizinho encontrado melhorou a solução
     improved = True
     while improved:
         improved = False
-        neighbourhood = aggregate(route, start)
+        # calcula a vizinhança
+        neighbourhood = aggregate(route, start) 
         for r, s in neighbourhood:
+            # precisamos verificar a validade da solução, caso seja inválida descarte
             if is_valid(r, s, demands, Q):
                 cost = calculate_cost(r, s, D)
+                # solução melhor do que a atual, continue buscando
                 if cost < best_cost:
                     improved = True
                     best_cost = cost
--- operators.py
+++ operators.py
@@ -2,8 +2,15 @@ import numpy as np
 from numba import njit, types
 
 @njit((types.int32[::1], types.boolean[::1]))
-def swap(route, start):
+def swap(route: np.ndarray, start: np.ndarray):
+    '''Troca dois nós de posição.
+
+    Gera uma lista de soluções vizinhas onde dois nós estão trocados de posição
+    na rota.
+    '''
     neighbourhood = []
+    # percorre todos os pares de nós e os troca, gerando uma solução vizinha
+    # possivelmente inválida, ou seja, que viole a restrição de capacidade
     for i in range(1, route.shape[0]):
         for j in range(i+1, route.shape[0]):
             new_route = route.copy()
@@ -16,15 +23,27 @@ def swap(route, start):
 
 @njit((types.int32[::1], types.boolean[::1]))
 def two_opt(route, start):
+    '''Remove duas arestas e reconecta a rota de forma alternativa.
+
+    Gera uma lista de soluções vizinhas onde foi aplicado o operador 2-opt.
+    '''
     neighbourhood = []
+    # encontra os índices no vetor onde são iniciadas as rotas
+    # precisamos percorrer cada rota individualmente
     route_starts = np.argwhere(start).flatten()
     for p in range(route_starts.shape[0]):
+        # índice do início da rota
         rs = route_starts[p]
+        # índice do final da rota
         re = route_starts[p+1] if p+1 < route_starts.shape[0] else route.shape[0]
         
+        # percorre todo par de arestas i, j na rota atual
         for i in range(rs+1, re):
+            # a aresta i é a aresta entre o vértice na posição i e o vértice na posição i-1
             for j in range(i+1, re):
+                # a aresta j é a aresta entre o vértice na posição j e o vértice na posição j-1
                 new_route = route.copy()
+                # inverte a rota entre os índices selecionados
                 new_route[i:j] = np.flip(new_route[i:j])
 
                 neighbourhood.append((new_route, start))
@@ -32,50 +51,31 @@ def two_opt(route, start):
     return neighbourhood
 
 
-@njit([(types.int32[::1], types.int64, types.int64), (types.boolean[::1], types.int64, types.int64)])
-def put_and_shift(array, i, j):
-    new = np.zeros_like(array)
-    new[j] = array[i]
-    a, b = min(i, j), max(i, j)
-    new[:a] = array[:a]
-    new[b+1:] = array[b+1:]
-    if i < j:
-        new[i:j] = array[i+1:j+1]
-    else:
-        new[j+1:i+1] = array[j:i]
-
-    return new
-
-
-@njit((types.int32[::1], types.boolean[::1]))
-def relocate(route, start):
-    neighbourhood = []
-
-    for i in range(1, route.shape[0]):
-        for j in range(1, route.shape[0]):
-            if i != j:
-                new_route = put_and_shift(route, i, j)
-                new_start = put_and_shift(start, i, j)
-                neighbourhood.append((new_route, new_start))
-    
-    return neighbourhood
-
-
 @njit((types.int32[::1], types.boolean[::1]))
 def aggregate(route, start):
+    '''Agrega o resultado das vizinhanças swap e 2-opt em uma única lista.
+
+    Gera uma lista de soluções vizinhas com as vizinhanças swap e 2-opt.
+    '''
     return swap(route, start) + two_opt(route, start)
-    # return relocate(route, start) + swap(route, start) + two_opt(route, start)
 
 
 # TABU variants
 
 @njit((types.int32[::1], types.boolean[::1]))
 def tabu_swap(route, start):
+    '''Troca dois nós de posição, retornando também os índices do movimento.
+
+    Gera uma lista de soluções vizinhas onde dois nós estão trocados de posição
+    na rota. A lista contém os índices i, j que indicam quais vértices foram
+    movimentados para gerar cada solução.
+    '''
     neighbourhood = []
     for i in range(1, route.shape[0]):
         for j in range(i+1, route.shape[0]):
             new_route = route.copy()
             new_route[i], new_route[j] = new_route[j], new_route[i]
+            # retorna os índices i, j para serem adicionados na lista tabu caso esse vizinho seja aceito
             neighbourhood.append((new_route, start, (i, j)))
     
     return neighbourhood
@@ -83,6 +83,13 @@ def tabu_swap(route, start):
 
 @njit((types.int32[::1], types.boolean[::1]))
 def tabu_two_opt(route, start):
+    '''Remove duas arestas e reconecta a rota de forma alternativa, retornando
+    também os índices do movimento.
+
+    Gera uma lista de soluções vizinhas onde foi aplicado o operador 2-opt.
+    A lista contém os índices i, j que indicam quais arestas foram
+    movimentadas para gerar cada solução.
+    '''
     neighbourhood = []
     route_starts = np.argwhere(start).flatten()
     for p in range(route_starts.shape[0]):
@@ -94,6 +101,7 @@ def tabu_two_opt(route, start):
                 new_route = route.copy()
                 new_route[i:j] = np.flip(new_route[i:j])
 
+                # retorna os índices i, j para serem adicionados na lista tabu caso esse vizinho seja aceito
                 neighbourhood.append((new_route, start, (i, j)))
     
     return neighbourhood
